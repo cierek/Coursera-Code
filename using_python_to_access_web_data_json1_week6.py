80e1c7f640d538a493eb56c568ac32d82462b9e2
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2017-08-17 09:00:21 +0100

    Update using_python_to_access_web_data_json1_week6.py
    From assignment - week 6 - JSON and the REST Architecture (Chapter 13). Purpose of program is to get data from the link, retrieve and sum all the "count" data. Code is a bit messy but works fine

--- using_python_to_access_web_data_json1_week6.py
+++ using_python_to_access_web_data_json1_week6.py
@@ -8,13 +8,14 @@ ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 
-
+#while is unecessary, input not rly working - code goes straight to the url if input not empy
 while True:
     address = input('Enter location: ')
     if len(address) < 1: break
     url = 'http://py4e-data.dr-chuck.net/comments_19182.json'
 
     print('Retrieving', url)
+    # opening the link and getting the data
     uh = urllib.request.urlopen(url)
     data = uh.read().decode()
     print('Retrieved', len(data), 'characters')
@@ -23,10 +24,10 @@ while True:
         js = json.loads(data)
     except:
         js = None
-
+    
     sum = 0
     #total_number = 0
-
+    #retrieving all count from comments and adding one by one (adding to sum)
     for comment in js["comments"]:
         sum += int(comment["count"])
         #total_number += 1
