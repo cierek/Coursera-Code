f28883097482c3fcda458126ec91eb71be2cef89
Piotr Cierkosz <piotr.w.cierkosz@icloud.com>
2017-08-17 10:05:44 +0100

    Add files via upload

---
+++ geojasonassig.py
@@ -0,0 +1,27 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+
+serviceurl = 'http://py4e-data.dr-chuck.net/geojson?'
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    url = serviceurl + urllib.parse.urlencode(
+        {'address': address})
+    uh = urllib.request.urlopen(url)
+    data = uh.read().decode()
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
+
+    lat = js["results"][0]["place_id"]
+
+    print('Place ID:', lat, )
